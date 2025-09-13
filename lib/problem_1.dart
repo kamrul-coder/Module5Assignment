5b0c368b0b015b24b869b7c7ac25bb6e7f147d15
Md Kamrul Hasan <itskamrul24@gmail.com>
2025-09-14 01:31:42 +0800

    init commit

---
+++ lib/problem_1.dart
@@ -0,0 +1,36 @@
+void main(){
+
+  // Book Object
+  Book book1 = Book('To Kill a Mockingbird', 'Harper Lee', 150);
+  Book book2 = Book('The Book Thief', 'Markus Zusak', 200);
+  book1.display(15);
+  book2.display(20);
+
+}
+
+class Book {
+  String ? title, author;
+  double ? price;
+
+  // Constructor
+  Book(this.title, this.author, this.price){
+
+  }
+
+  // Discount Method
+  double discountedPrice(double discountPercent){
+    return price! - price! * discountPercent /100;
+  }
+
+  // Display Book Details
+  display(double discountPercent){
+    print("Title: $title");
+    print("Author: $author");
+    print("Regular Price: $price Taka");
+    print("Discounted Price ($discountPercent%): ${discountedPrice(discountPercent)} Taka");
+    print("=====================");
+
+  }
+
+
+}
